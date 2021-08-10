84f207579f5c2c66df8893ee28c2639fa5717e9e
Sri Aspari <mail@siarie.me>
2021-08-10 10:18:31 +0700

    01: Introduction

---
+++ 01-introduction/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/*
+ * multiple line comment
+ *
+ * this function return "Hello, universe."
+ */
+func main() {
+     // inline comment
+     // fmt function from Println package
+     fmt.Println("Hello, universe")
+}
